9b4cc4f07aadc15d46a874d563611921151b99f9
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-09-16 21:58:40 -0500

    casi el header

--- src/js/ht.js
+++ src/js/ht.js
@@ -1,1 +1,9 @@
-const imgRes = document.getElementById("img-res");
+
+// const divimg = document.getElementById('img-res');
+// const imgRes = document.createElement('img');
+// imgRes.setAttribute('src', '../images/perfil.jpg')
+// divimg.append(img);
+
+const c = document.getElementById('crear');
+
+c.innerHTML = "hola desde javascript";
