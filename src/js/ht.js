632cec96a9c0c89b942986d0d7cd72e16fe6cdc2
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-09-17 23:31:13 -0500

    casi el nav

--- src/js/ht.js
+++ src/js/ht.js
@@ -11,3 +11,17 @@
 const j = document.createElement('img');
 j.setAttribute('src', '/proyectoPaginaWeb/src/images/perfil.jpg')
 c.append(j); */
+
+//creando la interaccion del nav
+
+function ver() {
+    document.getElementById('list').style.display = "block"
+    document.getElementById('logoyes').style.display="flex";
+    document.getElementById('logo-nav').style.display = "none";
+
+}
+function ocultar() {
+    document.getElementById('list').style.display = "none"
+    document.getElementById('logoyes').style.display = "none";
+    document.getElementById('logo-nav').style.display = "flex";
+}
