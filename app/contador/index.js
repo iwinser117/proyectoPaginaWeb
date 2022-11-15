95ee29c2a7fd40870ff9b542ef9ee016e66fe657
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-11-15 11:15:05 -0500

    hecho contador faltan estilos

--- app/contador/index.js
+++ app/contador/index.js
@@ -1,24 +1,16 @@
-let limite = 10;
-let a = 0;
-let i = 1;
-/* for (let a = 0; a < limite; a ++) {
-    document.write(`contador vale  ${a} <br/>`)
-    i = a;
-} */
-
-
+const h2 = document.querySelector("h2");
 let cActual = 0;
-function sumar () {
-    let o = 0
-    let r = o + 1
-    
-    cActual += r
-console.log(cActual)
-h2.innerHTML=`contador vale actualmete  ${cActual}`;
-    return cActual;
+function sumar() {
+  let o = 0;
+  let r = o + 1;
+  cActual += r;
+  h2.innerHTML = `${cActual}`;
+  return cActual;
 }
-
-
-const h2 = document.querySelector('h2');
-
-
+function restar (){
+    let o = 0;
+  let r = o - 1;
+  cActual += r;
+  h2.innerHTML = `${cActual}`;
+  return cActual;
+}
