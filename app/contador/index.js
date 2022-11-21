068c3d270a92fe4ab0edfd110eba8f42063ef378
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-11-21 08:39:10 -0500

    ok contador

--- app/contador/index.js
+++ app/contador/index.js
@@ -1,5 +1,13 @@
 const h2 = document.querySelector("h2");
+const resetContador = document.querySelector("reset");
+
 let cActual = 0;
+
+function resetear () {
+  cActual = 0;
+  return h2.innerHTML = 0;
+}
+
 function sumar() {
   let o = 0;
   let r = o + 1;
