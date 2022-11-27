f873e8c1d95a3883c2ac691264978c9657a18d23
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-11-27 17:01:28 -0500

    perfecto

--- app/contador/contador.js
+++ app/contador/contador.js
@@ -1,24 +1,24 @@
-const h2 = document.querySelector("h2");
+const dateActual = document.getElementById('verContador');
 const resetContador = document.querySelector("reset");
 
 let cActual = 0;
 
 function resetear () {
   cActual = 0;
-  return h2.innerHTML = 0;
+  return dateActual.innerHTML = 0;
 }
 
-function sumar() {
+function sumarUnoContador() {
   let o = 0;
   let r = o + 1;
   cActual += r;
-  h2.innerHTML = `${cActual}`;
+  dateActual.innerHTML = `${cActual}`;
   return cActual;
 }
-function restar (){
+function restarUnoContador (){
     let o = 0;
   let r = o - 1;
   cActual += r;
-  h2.innerHTML = `${cActual}`;
+  dateActual.innerHTML = `${cActual}`;
   return cActual;
 }
