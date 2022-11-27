888db8e6574078144c8b48773771181a00b4dc22
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-11-27 09:27:28 -0500

    funcional pallet

--- src/colorpicker/app/colr.js
+++ src/colorpicker/app/colr.js
@@ -1,14 +1,20 @@
-/* Selecting the input element with the id of color. */
-const color = document.querySelector("#color");
-/* Selecting the element with the id of salida. */
-const seleccion = document.querySelector("#salida");
+const color = document.getElementById('color');
+const bgPalet = document.getElementById('cont-palet')
+const seleccion = document.getElementById("salida");
 
-/* Listening for an input event and then changing the background color of the div to the color
-selected. */
-color.addEventListener("input" , () => {
-    const seleccion = color.value;
-    salida.innerHTML = seleccion;
-    salida.style.background = seleccion;
-    salida.style.color = "#fff";
-    // console.log(seleccion);
-})
+const almacenamiento = localStorage.getItem("valorColor") || "#ffffff";
+color.value = almacenamiento;
+
+const setColor = () => {
+  const value = color.value;
+  salida.innerHTML = value;
+  bgPalet.style.background = value;
+};
+const saveColors = (value) => {
+  localStorage.setItem("valorColor", value);
+};
+
+setColor();
+
+color.addEventListener("input", setColor);
+color.addEventListener("change", () => saveColors(color.value));
