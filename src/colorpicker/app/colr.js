2dba2cc98fcf21a2bcae3e5e6f1c25bd0a6ebadc
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2022-12-25 12:27:35 -0500

    upa

--- src/colorpicker/app/colr.js
+++ src/colorpicker/app/colr.js
@@ -1,5 +1,8 @@
 const color = document.getElementById("color");
 const bgPalet = document.getElementById("cont-palet");
+const clses = document.getElementsByClassName("cir-palet");
+
+
 const seleccion = document.getElementById("salida");
 
 const almacenamiento = localStorage.getItem("valorColor") || "#ffffff";
@@ -8,13 +11,16 @@ color.value = almacenamiento;
 const setColor = () => {
   const value = color.value;
   salida.innerHTML = value;
-  bgPalet.style.background = value;
+  // bgPalet.style.background = value;
+  for(item of clses){
+    item.style.background = value;
+  }
 };
 const saveColors = (value) => {
   localStorage.setItem("valorColor", value);
 };
 
 setColor();
-
+console.log(clses);
 color.addEventListener("input", setColor);
 color.addEventListener("change", () => saveColors(color.value));
