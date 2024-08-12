a25db73516c2d743234898cadd1c4388d03e8ac6
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2024-08-11 21:27:28 -0500

    ajuste redireccionamiento nueva version

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 hola, Bienvenido en este repositorio estoy desarrollando mi proyecto de pagina web haciendo uso de html, Css y Javascript. 
 
+existe una versión en https://iwinsersanchez.netlify.app
+
 Cada dia mejorando se realiza commits que van con el fin de mejorar la pagina web.
 ![Alt text](src/images/ok.svg)
--- src/js/ht.js
+++ src/js/ht.js
@@ -1,71 +1,73 @@
-
-// const divimg = document.getElementById('img-res');
-// const imgRes = document.createElement('img');
-// imgRes.setAttribute('src', '../images/perfil.jpg')
-// divimg.append(img);
-
-
-//ya crea la imagen a conformidad.
-/* const c = document.getElementById('crear');
-
-const j = document.createElement('img');
-j.setAttribute('src', '/proyectoPaginaWeb/src/images/perfil.jpg')
-c.append(j); */
-
-//creando la interaccion del nav
-
 function ver() {
-    document.getElementById('list').style.display = "block" ;
-    document.getElementsByClassName('logoNav').style.display="flex";
-    document.getElementsByClassName('logoNav').style.display = "none";
+  document.getElementById("list").style.display = "block";
+  document.getElementsByClassName("logoNav").style.display = "flex";
+  document.getElementsByClassName("logoNav").style.display = "none";
 }
 function enviar(e) {
-    console.log(e);
-    e.preventDefault();
-    alert("Datos enviados");
-    e.target.reset();
-  }
+  console.log(e);
+  e.preventDefault();
+  alert("Datos enviados");
+  e.target.reset();
+}
 function ocultar() {
-    document.getElementById('list').style.display = "none"
-    document.getElementsByClassName('logoClosed').style.display = "none";
-    document.getElementsByClassName('logoNav').style.display = "flex";
+  document.getElementById("list").style.display = "none";
+  document.getElementsByClassName("logoClosed").style.display = "none";
+  document.getElementsByClassName("logoNav").style.display = "flex";
 }
 function ocultarformulario() {
-    document.getElementById('cont-form').style.display = "none";
+  document.getElementById("cont-form").style.display = "none";
 }
 function mostrarFormulario() {
-    document.getElementById('cont-form').style.display = "block";
+  document.getElementById("cont-form").style.display = "block";
 }
 
+let mostrarTextBtn = document.getElementById("mostrarTextBtn");
+let mostrarText = document.getElementById("mostrarText");
 
+mostrarTextBtn.addEventListener("click", toogleMostrarText);
 
-let mostrarTextBtn = document.getElementById('mostrarTextBtn');
-let mostrarText = document.getElementById('mostrarText');
-
-mostrarTextBtn.addEventListener('click', toogleMostrarText);
-
-function toogleMostrarText(){
-    mostrarText.classList.toggle('verList')
-    if(mostrarText.classList.contains('verList')){
-        mostrarTextBtn.innerHTML = 'Ver menos...'
-    }else{
-        mostrarTextBtn.innerHTML = '...Ver mas'
-    }
+function toogleMostrarText() {
+  mostrarText.classList.toggle("verList");
+  if (mostrarText.classList.contains("verList")) {
+    mostrarTextBtn.innerHTML = "Ver menos...";
+  } else {
+    mostrarTextBtn.innerHTML = "...Ver mas";
+  }
 }
 
-function verDiv (){
-    document.getElementById('divOculto').style.display = "block";
+function verDiv() {
+  document.getElementById("divOculto").style.display = "block";
 }
 function ocultarDiv() {
-    document.getElementById('divOculto').style.display = 'none';
+  document.getElementById("divOculto").style.display = "none";
 }
 
-function menuMostrar (){
-    document.getElementById('list').style.display = 'flex';
-    document.getElementById('btn-peque').style.display = 'none';    
+function menuMostrar() {
+  document.getElementById("list").style.display = "flex";
+  document.getElementById("btn-peque").style.display = "none";
 }
-function menuOcultar (){
-    document.getElementById('list').style.display = 'none';
-    document.getElementById('btn-peque').style.display = 'flex';
+function menuOcultar() {
+  document.getElementById("list").style.display = "none";
+  document.getElementById("btn-peque").style.display = "flex";
 }
 
+function goToNewVersion() {
+  window.location.href = "https://iwinsersanchez.netlify.app";
+}
+
+function stayHere() {
+  const notification = document.getElementById("notification");
+  notification.classList.remove("show");
+  notification.classList.add("hidden");
+}
+
+setTimeout(() => {
+  const notification = document.getElementById("notification");
+  notification.classList.remove("hidden");
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+    notification.classList.add("hidden");
+  }, 15000);
+}, 2000);
