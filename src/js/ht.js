69b035f2c03a0be8bb89cc85655e67839758822a
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2024-12-01 22:32:15 -0500

    "Updated notification styles and behavior in ht.js and footer.css"

--- src/js/ht.js
+++ src/js/ht.js
@@ -62,12 +62,13 @@ function stayHere() {
 }
 
 setTimeout(() => {
+  
   const notification = document.getElementById("notification");
   notification.classList.remove("hidden");
   notification.classList.add("show");
 
-  setTimeout(() => {
-    notification.classList.remove("show");
-    notification.classList.add("hidden");
-  }, 15000);
+    // setTimeout(() => {
+    //   notification.classList.remove("show");
+    //   notification.classList.add("hidden");
+    // }, 15000);
 }, 2000);
